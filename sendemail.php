2793d40e1730af0b7559c363f924133f214e820a
anjangowdamandya <anjangowdamandya@gmail.com>
2017-01-10 22:59:15 +0530

    added by kiran

--- sendemail.php
+++ sendemail.php
@@ -4,13 +4,13 @@
 $name = @trim(stripslashes($_POST['name'])); 
 $email = @trim(stripslashes($_POST['email'])); 
 $phone = @trim(stripslashes($_POST['phone'])); 
-$subject = @trim(stripslashes($_POST['subject'])); 
-$message = @trim(stripslashes($_POST['message'])); 
+$subject = "User interested"; 
+
 
 $email_from = $email;
-$email_to = 'enquiries@eternogroups.com';//replace with your email
+$email_to = 'sales@eternogroups.com';//replace with your email
 
-$body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Phone: ' . $phone . "\n\n"  . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
+$body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Phone: ' . $phone . "\n\n"  .
 
 mail($email_to,$subject,$body);
 
